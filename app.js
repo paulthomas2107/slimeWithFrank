5d9a9d69f36e2a4ee454279f43d7ceb04ddd7e03
Paul Thomas <paulthomas2107@gmail.com>
2022-10-21 10:06:29 +0100

    More.....

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.fillStyle = "white";
 
 class Ball {
     constructor(effect) {
         this.effect = effect;
         this.x = this.effect.width * 0.5;
         this.y = this.effect.height * 0.5;
-        this.radius = 50;
-        this.speedX = 1;
-        this.speedY = 1;
+        this.radius = Math.random() * 80 + 20;
+        this.speedX = Math.random() - 0.5;
+        this.speedY = Math.random() - 0.5;
     }
     update() {
+        if (this.x < this.radius || this.x > this.effect.width - this.radius) this.speedX *= -1;
+        if (this.y < this.radius || this.y > this.effect.height - this.radius) this.speedY *= -1;
         this.x += this.speedX;
         this.y += this.speedY;
     }
@@ -42,9 +45,14 @@ class MetaballsEffect {
     }
 }
 
-const effect = new MetaballsEffect();
+const effect = new MetaballsEffect(canvas.width, canvas.height);
+effect.init(20);
 
 function animate() {
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.update();
+    effect.draw(ctx);
+    requestAnimationFrame(animate)
 }
+animate();
 
